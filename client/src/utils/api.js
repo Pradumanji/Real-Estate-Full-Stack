52eabc9c3c6255fa86129ed55230766051b55f63
Praduman-gupta <103199278+Pradumanji@users.noreply.github.com>
2023-09-28 12:32:34 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "https://full-stack-real-estate-youtube.vercel.app/api",
+  baseURL: "https://real-estate-full-stack.vercel.app/api",
 });
 
 export const getAllProperties = async () => {
